4dc8c94def527f6e31e604a301b3aeaf43038396
Sichan Yuh <sichan.yuh@gmail.com>
2020-12-30 18:19:29 -0500

    merge sort working

--- src/algosAndAni/sortAlgos.js
+++ src/algosAndAni/sortAlgos.js
@@ -1,14 +1,88 @@
 //merge sort animation
-export function getMergeSortAni(arr) {
-
+export function mergeSortAni(arr) {
+ //array of pair of indices compared, in order
+ const ani = [];
+ //auxiliary array that acts as original copy to refer to in merging
+ //and getting indices compared
+ const aux = arr.slice();
+ //merge sort arr from 0 to length - 1, and store indices compared in ani
+ mergeSort(arr, 0, arr.length - 1, aux, ani);
+ //return the pairs of indices compared
+ return ani;
 }
-//merge sort algorithms
-function mergeSort(props) {
 
+function mergeSort(
+ main, start, end, aux, ani,
+) {
+ //if start is greater than or equal to end, end sort
+ //since that means start is past or at mid index, meaning
+ //the pseudo-array (pseudo since we didn't really divide anything)
+ //at hand is of length 1, which is when to stop recursion and start merging
+ if (start === end) return;
+ const mid = Math.floor((start+end)/2);
+
+ //aus and main are flipped here, compared to argument list at function header
+ mergeSort(aux, start, mid, main, ani);
+ mergeSort(aux, mid+1, end, main, ani);
+ 
+ merge(main, start, mid, end, aux, ani);
 }
 
-function merge(props) {
+function merge (main, start, mid, end, aux, ani,) {
+ //m here is the index value for main array
+ let m = start;
+ //i here will take on index value from 0 to mid
+ let i = start;
+ //j here will take on index value from mid + 1 to end
+ let j = mid + 1;
 
+ //first copy in values from left half of aux and right half of aux
+ //into the main array, based on which element is greater
+ //each time an element is copied, increase m by one (index for main array)
+ while (i < mid + 1 && j < end + 1) {
+  //this is used to toggle comparison colors between bars
+  ani.push([i, j]);
+  ani.push([i, j]);
+
+  if (aux[i] <= aux[j]) {
+   //store the new height at index m, by pushing m, aux[i] to ani
+   ani.push([m, aux[i]]);
+   //copy in value at aux[i] to main[m] and increase i
+   main[m] = aux[i++];
+  } else {
+   //store the new height at index m, by pushing m, aux[j] to ani
+   ani.push([m, aux[j]]);
+   //copy in value at aux[j] to main[m] and increase j
+   main[m] = aux[j++];
+  }
+  //increase index to main array by one, to move onto next spot to 
+  //put an element into
+  m++;
+ }
+
+ //now its time to add in any remaining elements into main
+ while (i < mid + 1) {
+  //animation toggle, again
+  ani.push([i, i]);
+  ani.push([i, i]);
+
+  //store the new height at index m, by pushing m, aux[i] to ani
+  ani.push([m, aux[i]]);
+
+  //add in elements
+  main[m++] = aux[i++];
+ }
+
+ while (j < end + 1) {
+  //toggle
+  ani.push([j, j]);
+  ani.push([j, j]);
+
+  //store the new height at index m, by pushing m, aux[i] to ani
+  ani.push([m, aux[j]]);
+
+  //add in elements
+  main[m++] = aux[j++];
+ }
 }
-
 
--- src/visualizer/sortAlgoVisu.jsx
+++ src/visualizer/sortAlgoVisu.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './SortAlgoVisu.css';
-import {getMergeSortAni} from '../algosAndAni/sortAlgos.js';
+import {mergeSortAni} from '../algosAndAni/sortAlgos.js';
 
 //interval between visual changes in ms
-const SPEED = 1;
+const SPEED = 3;
 
 //number of values to sort in array
 const BARS = 250;
 
 //default color of bars
-const DEFAULT_COLOR = 'black';
+const DEFAULT_COLOR =  'rgb(161, 161, 161)';
 
 //color of bars during comparison
-const COMP_COLOR = 'blue';
+const COMP_COLOR = 'red';
 
 //color of bars at sorted position
 const END_COLOR = 'green';
@@ -45,7 +45,38 @@ export default class SortAlgoVisu extends React.Component {
         sort algo methods 
     *///////////////////////////////////////
     mergeSort() {
+        //array of indices compared to recreate sorting in real time animation
+        const ani = mergeSortAni(this.state.arr);
 
+        //loop through all index comparisons in ani
+        for (let i = 0; i < ani.length; i++) {
+            //get the html collection of bars
+            const bars = document.getElementsByClassName('arr-bar');
+            //boolean that tells us that at every 3rd value we change heights
+            //and at 1st and 2nd we toggle colors
+            const toggle = i % 3 != 2;
+
+            //if toggle is 0 or 1, then it is just changing color time
+            if (toggle) {
+                //get the indices of two bars being compared at this instant
+                const [one, two] = ani[i];
+                //if i % 3 is 0, that means color has to go from default to comp since current color is default (at beginning of comparison)
+                //if i % 3 is 1, that means color has to go from comp to default since current color is comp (at end of comparison)
+                const color = i % 3 === 0 ? COMP_COLOR : DEFAULT_COLOR;
+                //toggle bar colors at set speed (constant at top)
+                setTimeout(() => {
+                    bars[one].style.backgroundColor = color;
+                    bars[two].style.backgroundColor = color;
+                }, i * SPEED);
+            } else {
+                //else here would mean that it is time to overwrite bar height
+                setTimeout(() => {
+                    const [idx, newHt] = ani[i];
+                    const barStyle = bars[idx].style;
+                    barStyle.height = newHt + 'px';
+                }, i * SPEED);
+            }
+        }
     }
 
     quickSort() {
@@ -64,7 +95,6 @@ export default class SortAlgoVisu extends React.Component {
 
     }
 
-
     //render method
     render() {
         const {arr} = this.state;
@@ -72,17 +102,19 @@ export default class SortAlgoVisu extends React.Component {
         return (
             <div className = "arr-container">
                 <br></br>
-                <button onClick={() => this.initArr()} className = "generate-button">Generate New Array</button>
-                &nbsp;
-                <button onClick={() => this.mergeSort()} className = "button">Merge Sort</button>
-                &nbsp;
-                <button onClick={() => this.quickSort()} className = "button">Quick Sort</button>
-                &nbsp;
-                <button onClick={() => this.heapSort()} className = "button">Heap Sort</button>
-                &nbsp;
-                <button onClick={() => this.bubbleSort()} className = "button">Bubble Sort</button>
-                &nbsp;
-                <button onClick={() => this.countingSort()} className = "button">Counting Sort</button>
+                <div className = "toolbar">
+                    <button onClick={() => this.initArr()} className = "generate-button">Generate New Array</button>
+                    &nbsp;
+                    <button onClick={() => this.mergeSort()} className = "button">Merge Sort</button>
+                    &nbsp;
+                    <button onClick={() => this.quickSort()} className = "button">Quick Sort</button>
+                    &nbsp;
+                    <button onClick={() => this.heapSort()} className = "button">Heap Sort</button>
+                    &nbsp;
+                    <button onClick={() => this.bubbleSort()} className = "button">Bubble Sort</button>
+                    &nbsp;
+                    <button onClick={() => this.countingSort()} className = "button">Counting Sort</button>
+                </div>
                 <br></br>
                 <br></br>
                 {arr.map((value, idx) => (
@@ -93,6 +125,8 @@ export default class SortAlgoVisu extends React.Component {
                             height: value, 
                     }}></div>
                 ))}
+                <br></br>
+                <br></br>
             </div>
         );
     }
